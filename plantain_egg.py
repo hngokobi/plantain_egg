2755da7b6108c477beec62fc6ef5d4f7352db3ac
hngokobi <hngokobi@gmail.com>
2020-08-18 20:00:48 -0400

    recipe assignment

---
+++ plantain_egg.py
@@ -0,0 +1,34 @@
+# stage 1: Data input 
+fried Plantains and egg
+- 5 eggs
+- 2 ripe Plantains
+- 1/4 onions
+- 1/4 teaspoon pepper (if u like spicy)
+- knife 
+- 1 maggie
+- a pinch of salt to taste
+- 60 ml of vegetable oil to fry the egg
+- 3 cups of vegetable oil to fry the plantains   
+- 1 plum tomato
+- fork
+# stage 2: Data processing
+- go to Giants and buy all the requirements
+- remove everything from the shopping bags
+- measure the required quantities of ingredients needed
+- usings the knife, peel and slice the ripe plantains
+- pour the 3 cups of oil into the pot and place on the stove
+- heat the oil to about 375 F
+- place the sliced plantains in the pot to fry for about 5 minutes
+  or untill the color changes to brown
+- remove the fried plantains from the oil using the fork
+- the plantains is now ready to serve  
+- crack ur eggs using a fork or knife
+- use ur knife to slice the tomato and onions
+- add a pinch of salt and maggie
+- use a fork to mix 
+- put the 60 ml of oil into the fry pan and heat to about 158*F 
+- pour the mixture into the fry pan and swirl 
+- allow for 45 seconds and flip
+- turn off the stove 
+# satge 3: Results
+serve your plantains and egg on plate 
